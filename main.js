50f162cc098448b514f45db748da2da2274fb7b8
osakdv <osakdv@gmail.com>
2023-09-26 16:44:41 +0100

    Updated

---
+++ main.js
@@ -0,0 +1,15 @@
+const project = [
+    {
+        imgLink: '/imgs/p1.jpeg',
+        title: 'Fintech App',
+        brief: 'A Ui design that push the cores of the brand',
+        link: 'https://dribbble.com/shots/17236464-Neobank-Landingpage-Hero/attachments/12341718?mode=media'
+    },
+
+    {
+        imgLink: '/imgs/p4.png',
+        title: 'Drag and drop gallery App',
+        brief: 'Allowing users to rearrange images and drap&drop',
+        link: 'https://dribbble.com/shots/17236464-Neobank-Landingpage-Hero/attachments/12341718?mode=media'
+    }
+]
