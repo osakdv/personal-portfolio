238a12d036ff00d0a070f9d5af96f1132f0ca23a
osakdv <osakdv@gmail.com>
2023-09-26 17:07:40 +0100

    btn controller

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const pTitle = document.getElementById("p-title")
+const pBrief = document.getElementById("p-brief")
+const pLink = document.getElementById("p-link")
+const pImg = document.getElementById("p-img")
+const prvBtn = document.querySelector(".prev-btn")
+const nextBtn = document.querySelector(".next-btn")
+
 const project = [
     {
         imgLink: '/imgs/p1.jpeg',
@@ -13,3 +20,27 @@ const project = [
         link: 'https://dribbble.com/shots/17236464-Neobank-Landingpage-Hero/attachments/12341718?mode=media'
     }
 ]
+
+let currentIndex = 0;
+
+const swipeThrough = (btn) => {
+    if(btn === 'next') {
+        currentIndex++
+        if(currentIndex >= project.length) return
+    } else if(btn === 'prev') {
+        currentIndex--
+        if(currentIndex <= 0) return
+    }
+    
+    pTitle.textContent = project[currentIndex].title
+    pBrief.textContent = project[currentIndex].brief
+    pImg.src = project[currentIndex].imgLink
+}
+
+swipeThrough('next')
+
+prvBtn.addEventListener("click", () => {
+    console.log("prv")
+    swipeThrough('prev')
+})
+nextBtn.addEventListener("click", () => swipeThrough('next'))
